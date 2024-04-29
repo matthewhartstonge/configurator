e76bdbc51677440d396fb3b08db3cbac96cc4fd2
Matthew Hartstonge <matt@mykro.co.nz>
2024-04-30 07:06:01 +1200

    feat(ConfigType): simplifies dependant use by implementing base functionality.

--- config_type.go
+++ config_type.go
@@ -1,14 +1,24 @@
 package configurator
 
+var _ ConfigImplementer = (*ConfigType)(nil)
+
+// ConfigType provides an abstract struct (née abstract base class) to compose
+// into concrete config parser implementations.
 type ConfigType struct {
-	Data ConfigImplementer
+	// Config provides
+	Config ConfigImplementer
 }
 
+// Validate expects the implementor to return any errors found in the parsed
+// configuration. If any errors are found it aims to provide clear user
+// instruction as to where errors were encountered and how one would go about
+// fixing it.
 func (c *ConfigType) Validate() error {
-	// base case no validation required
-	return nil
+	return c.Config.Validate()
 }
 
-func (c *ConfigType) Merge(config any) any {
-	panic("implement me")
+// Merge expects the implementor to merge the parsed configuration from the
+// concrete implementation and bind it back into the provided domain config.
+func (c *ConfigType) Merge(domainConfig any) any {
+	return c.Config.Merge(domainConfig)
 }
